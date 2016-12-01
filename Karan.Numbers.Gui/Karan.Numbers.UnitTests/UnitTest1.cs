900a73a10d44dccbee2ab7064b06295fecba4ba6
Brian Snyder <bsnyder@technology4retailers.com>
2016-12-01 10:16:31 -0600

    initial commit

--- Karan.Numbers.Gui/Karan.Numbers.UnitTests/UnitTest1.cs
+++ Karan.Numbers.Gui/Karan.Numbers.UnitTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Karan.Numbers.UnitTests
 {
@@ -21,7 +22,21 @@ namespace Karan.Numbers.UnitTests
         [TestMethod]
         public void TestChangeReturnsCorrectAmount()
         {
+            decimal Cost = 2.26M;
+            decimal MoneyTendered = 5M;
 
+            Assert.AreEqual(2.74M, Change.Due(Cost, MoneyTendered));
+        }
+
+        [TestMethod]
+        public void TestRuturnCorrectMonies()
+        {
+            Dictionary<string, int> Result = Change.DueMonies(5.74M);
+
+            Assert.AreEqual(Result["FiveDollar"], 1);
+            Assert.AreEqual(Result["Quarter"], 2);
+            Assert.AreEqual(Result["Dime"], 2);
+            Assert.AreEqual(Result["Penny"], 4);
         }
     }
 }
---
+++ Karan.Numbers.Gui/Karan.Numbers/Change.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karan.Numbers
+{
+    public static class Change
+    {
+        enum Currency
+        {
+            TwentyDollar = 2000,
+            TenDollar = 1000,
+            FiveDollar = 500,
+            Dollar = 100,
+            Quarter = 25,
+            Dime = 10,
+            Nickle = 5,
+            Penny = 1,
+        }
+
+        public static decimal Due(decimal cost, decimal amountTendered)
+        {
+            return amountTendered - cost;
+        }
+
+        public static Dictionary<string, int> DueMonies(decimal changeDue)
+        {
+            Dictionary<String, int> result = new Dictionary<string, int>();
+            int Remainder = (int)(changeDue * 100);
+            int Money;
+
+            foreach (var currency in Enum.GetValues(typeof(Currency)).Cast<Currency>().OrderByDescending(x => x))
+            {
+                Money = Remainder / (int)currency;
+                if (Money > 0)
+                {
+                    Currency value = (Currency)currency;
+                    result.Add(value.ToString(), Money);
+                    Remainder -= Money * (int)currency;
+                }    
+            }
+            return result;
+        }
+    }
+}
